505b6fc3cfeda441599e37cf9bf5af9cf9ae39a5
WoodMath <jeffwood314@gmail.com>
2016-05-23 13:14:24 -0500

    Updated bash scripts for OS-dependant viewer execution

--- notes/latex.sh
+++ notes/latex.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# From:
+#	http://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linux
 for CODE in $@
 do
 
@@ -16,13 +18,28 @@ do
 #cd ./Dropbox/cse3320/Assignment05/lab05
 #cd ~/Dropbox/rt_proj/deadmau5
 #./usr/share/doc/povray/examples/
-pdflatex $CODE.tex
+	pdflatex $CODE.tex
 #bibtex $CODE.aux
 #pdflatex $CODE.tex
 #pdflatex $CODE.tex
-evince $CODE.pdf
+#evince $CODE.pdf
 
 #export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+
+
+	if [ "$(uname)" == "Darwin" ]; then
+		# Do something under Mac OS X platform        
+		echo "Operating system is $(uname)"
+		open $CODE.pdf
+	elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+		# Do something under GNU/Linux platform
+		echo "Operating system is $(uname)"
+		evince $CODE.pdf
+	elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+		# Do something under Windows NT platform
+		echo "Operating system is $(uname)"
+	fi
+
 done
 #ls -l -r -t
 
--- playground/bib.sh
+++ playground/bib.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# From:
+#	http://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linux
+
 for CODE in $@
 do
 
@@ -16,11 +19,24 @@ do
 #cd ./Dropbox/cse3320/Assignment05/lab05
 #cd ~/Dropbox/rt_proj/deadmau5
 #./usr/share/doc/povray/examples/
-pdflatex $CODE.tex
-bibtex $CODE.aux
-pdflatex $CODE.tex
-pdflatex $CODE.tex
-#evince $CODE.pdf
+	pdflatex $CODE.tex
+	bibtex $CODE.aux
+	pdflatex $CODE.tex
+	pdflatex $CODE.tex
+
+	if [ "$(uname)" == "Darwin" ]; then
+		# Do something under Mac OS X platform        
+		echo "Operating system is $(uname)"
+		open $CODE.pdf
+	elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+		# Do something under GNU/Linux platform
+		echo "Operating system is $(uname)"
+		evince $CODE.pdf
+	elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+		# Do something under Windows NT platform
+		echo "Operating system is $(uname)"
+	fi
+
 
 #export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
 done
