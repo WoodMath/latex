3f0b32385dfccb6d1150997deb1a0c09b2061ee2
WoodMath <jeffwood314@gmail.com>
2016-07-12 22:52:05 -0500

    Updated thesis document

--- copy.sh
+++ copy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-LOCATION="/home/jeff/Dropbox/Wood_Kamangar/thesisORarticle/"
-cp ./thesisORarticle/thesisORarticle.pdf $LOCATION
+LOCATION="/home/jeff/Dropbox/Wood_Kamangar/thesis/"
+cp ./ms_thesis/ms_thesis.pdf $LOCATION
 cp ./researchlog/researchlog.pdf $LOCATION
 
 #cp ./thesisORarticle/thesisORarticle.pdf /home/jeff/Dropbox/Wood_Kamangar/thesisORarticle/
