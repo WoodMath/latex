820e396453dd307910fc7bc8283b53ff1a73ce1a
WoodMath <jeffwood314@gmail.com>
2016-07-28 08:39:01 -0500

    Seperated research log out into weeks

--- copy.sh
+++ copy.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-LOCATION="/home/jeff/Dropbox/Wood_Kamangar/thesis/"
-cp ./ms_thesis/ms_thesis.pdf $LOCATION
-cp ./researchlog/researchlog.pdf $LOCATION
+STATUS_LOCATION="/home/jeff/Dropbox/Wood_Kamangar/StatusReports/"
+THESIS_LOCATION="/home/jeff/Dropbox/Wood_Kamangar/thesis/"
+
+cp ./researchlog/researchlog.pdf $STATUS_LOCATION
+cp ./ms_thesis/ms_thesis.pdf $THESIS_LOCATION
 
 #cp ./thesisORarticle/thesisORarticle.pdf /home/jeff/Dropbox/Wood_Kamangar/thesisORarticle/
 
